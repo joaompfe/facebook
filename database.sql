8ce9ff84e75ff9c8c02fd4e5ade677a4cba8ad95
joaompfe <joaofe2000@gmail.com>
2020-04-09 23:14:52 +0100

    login page html and css

--- database.sql
+++ database.sql
@@ -4,19 +4,53 @@ USE facebook;
 CREATE TABLE persons
 (
 	id 			INT AUTO_INCREMENT,
-	name 		VARCHAR(60) NOT NULL,
+    firstName 	VARCHAR(30) NOT NULL,
+    lastName 	VARCHAR(30) NOT NULL,
+	fullName 	VARCHAR(60) AS (CONCAT(firstName, " ", lastName)),
     email 		VARCHAR(100) NOT NULL,
     password 	VARCHAR(50) NOT NULL,
-    CONSTRAINT pk_persons PRIMARY KEY (id)
+    birthday	DATE NOT NULL,
+    gender 		CHAR(1),
+    CONSTRAINT pk_persons PRIMARY KEY (id),
+    CONSTRAINT ck_persons_gender CHECK(gender = 'M' OR gender = 'F')
 );
 
 CREATE TABLE posts
 (
 	id INT AUTO_INCREMENT,
     author INT,
-    creation_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    creationTime DATETIME DEFAULT NOW(),
     text TEXT,
     likes INT DEFAULT 0,
     CONSTRAINT pk_posts PRIMARY KEY (id),
-    CONSTRAINT fk_posts_author FOREIGN KEY (author) REFERENCES persons (id)
+    CONSTRAINT fk_postsAuthor FOREIGN KEY (author) REFERENCES persons (id)
 );
+
+CREATE TABLE postComments 
+(
+	id INT AUTO_INCREMENT,
+    author INT,
+    creationTime DATETIME DEFAULT NOW(),
+    text VARCHAR(300),
+    likes INT DEFAULT 0,
+    post INT,
+    CONSTRAINT pk_postComments PRIMARY KEY (id),
+    CONSTRAINT fg_postComments_author FOREIGN KEY (author) REFERENCES persons (id),
+    CONSTRAINT fg_postComments_post FOREIGN KEY (post) REFERENCES posts (id)
+);
+
+CREATE TABLE commentComments 
+(
+	id INT AUTO_INCREMENT,
+    author INT,
+    creationTime DATETIME DEFAULT NOW(),
+    text VARCHAR(300),
+    likes INT DEFAULT 0,
+    comment INT,
+    CONSTRAINT pk_commentComments PRIMARY KEY (id),
+    CONSTRAINT fg_commentComments_author FOREIGN KEY (author) REFERENCES persons (id),
+    CONSTRAINT fg_commentComments_comment FOREIGN KEY (comment) REFERENCES postComments (id)
+);
+
+INSERT INTO persons (firstName, lastName, email, password, birthday, gender)
+VALUES ('João', 'Fé', 'jf@sapo.pt', '123', '2000-10-25', 'M');
--- server/client.php
+++ server/client.php
@@ -5,11 +5,9 @@ require_once 'Client.php';
 
 if (!isset($_SESSION["client"])) {
     echo "{}";
-    error_log("PHP CLIENT NO SESSION");
 }
 else if (isset($_SESSION["client"])) {
     echo json_encode($_SESSION["client"]);
-    error_log("PHP CLIENT SESSION AND CLIENT");
 }
 else {
     error_log("PHP CLIENT SESSION AND NO CLIENT");
